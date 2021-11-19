1e8843c58c4b851218f6cf650624644ea63e4da6
Lucas Gaudet <lgaudet@chapman.edu>
2021-11-18 21:11:12 -0800

    Added gravity on the Corgi and Bounce off of platforms

---
+++ Assets/Scripts/Corgi.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* TO DO:
+    - move left or right with arrow keys
+    - gravity only affects y velocity 
+
+*/
+
+
+public class Corgi : MonoBehaviour
+{
+    public SpriteRenderer CorgiSpriteRenderer;
+    public SpriteTools SpriteTools;
+
+    private float initialYVelocity = GameParameters.InitialYVelocity;
+
+    private bool IsCorgiBelowMidScreen = true;
+
+    public void Update()
+    {
+        ApplyGravity(CorgiSpriteRenderer);
+    }
+
+
+    public void Move(Vector2 direction)
+    {
+        Vector2 amountToMove = CalculateAmountToMoveX(direction);
+        CorgiSpriteRenderer.transform.Translate(amountToMove);
+
+    }
+
+    public void MoveWithKeyboard(Vector2 direction)
+    {
+        Move(direction);
+    }
+
+    private Vector2 CalculateAmountToMoveX(Vector2 direction)
+    {
+        float amountX = GameParameters.CorgiMoveDistanceX * direction.x;
+
+        return new Vector2(amountX, 0);
+    }
+
+    private bool IsCorgiMoveableInY()
+    {
+        if (SpriteTools.IsBelowMidScreen(CorgiSpriteRenderer))
+            IsCorgiBelowMidScreen = true;
+        else
+            IsCorgiBelowMidScreen = false;
+
+        return IsCorgiBelowMidScreen;
+    }
+
+    private void ApplyGravity(SpriteRenderer spriteRenderer)
+    {
+        //if (IsCorgiMoveableInY())
+            spriteRenderer.transform.Translate(new Vector2(0, CalculateYVelocityDueToGravity()));
+    }
+
+    private float CalculateYVelocityDueToGravity()
+    {
+        // Vf = Vi - gt
+        float finalYVelocity = initialYVelocity - GameParameters.GravityStrength;
+        initialYVelocity = finalYVelocity;
+        return finalYVelocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            initialYVelocity += GameParameters.PlatformBoostYVelocity;
+            print("bruh");
+        }
+    }
+}
---
+++ Assets/Scripts/GameParameters.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameParameters
+{
+    public static float CorgiMoveDistanceX = 0.01f;
+    public static float GravityStrength = 0.0005f;
+    public static float InitialYVelocity = 0.05f;
+    public static float PlatformBoostYVelocity = 0.08f;
+
+
+}
