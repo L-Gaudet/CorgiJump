71516db0c39b3a221a4183dbd9eb709d242064ed
kianajung11 <kjung@chapman.edu>
2021-12-13 23:21:03 -0800

    moving platform & enemy animation
    there is a bug with the moving platforms

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    private bool moveRight = true;
+    public float maxX;
+    public float moveSpeed;
+
+    void Update()
+    {
+        if (transform.position.x > maxX)
+            moveRight = false;
+        if (transform.position.x < -maxX)
+            moveRight = true;
+
+        if (moveRight)
+            transform.position = new Vector2(transform.position.x + moveSpeed + Time.deltaTime, transform.position.y);
+        else
+            transform.position = new Vector2(transform.position.x - moveSpeed + Time.deltaTime, transform.position.y);
+    }
+}
