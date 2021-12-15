229508ee150ea077c2218deb8c3343ecce080fff
Lucas Gaudet <lgaudet@chapman.edu>
2021-12-14 20:49:12 -0800

    added clouds

--- Assets/Scripts/Cloud.cs
+++ Assets/Scripts/Cloud.cs
@@ -4,15 +4,112 @@ using UnityEngine;
 
 public class Cloud : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject backgroundCloud;
+    //public int maxNumberOfClouds = 3;
+    public float minimumYDistance = 5f;
+    public float maximumYDistance = 10f;
+    public int numberOfClouds = 5;
+
+    private bool isMoving = false;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isMoving)
+        {
+
+            //MovePlatforms();  // just for testing
+            UpdateClouds();
+        }
+
+    }
+
+    //public void CreateRandomCloud()
+    //{
+    //    float randomX = Random.Range(-4f, 4f);
+    //    float randomZ = Random.Range(0.1f, 10f);
+    //    //float randomY = Random.Range(minimumYDistance, maximumYDistance) + lastPlatformY;
+    //    Vector3 placement = new Vector3(randomX, 15f , randomZ);
+
+    //}
+
+    public void CreateInitialClouds()
+    {
+        isMoving = true;
+
+        float lastCloudHeight = -4f;
+        SpawnCloud(lastCloudHeight);
+
+        for (int i = 0; i < numberOfClouds; i++)
+        {
+            lastCloudHeight = SpawnCloud(lastCloudHeight);
+        }
+    }
+
+    public float SpawnCloud(float lastCloudY)
+    {
+        float randomX = Random.Range(-4f, 4f);
+        float randomY = Random.Range(minimumYDistance, maximumYDistance) + lastCloudY;
+        float randomZ = Random.Range(0.1f, 10f);
+
+
+        Instantiate(backgroundCloud, new Vector3(randomX, randomY, randomZ), Quaternion.identity);
+
+        return randomY;
+    }
+
+    public void UpdateClouds()
+    {
+        // get all the clouds
+        GameObject[] cloudObjects = GameObject.FindGameObjectsWithTag("Cloud");
+
+        // if a platform is too far off bottom screen
+        for (int i = 0; i < cloudObjects.Length; i++)
+        {
+            if (IsTooFarOffScreen(cloudObjects[i]))
+            {
+                DeletePlatform(cloudObjects[i]);
+                AddNewCloud(cloudObjects);
+            }
+        }
+    }
+
+    private bool IsTooFarOffScreen(GameObject cloudObject)
+    {
+        if (cloudObject.transform.position.y < Camera.main.transform.position.y - 10)
+            return true;
+        return false;
+    }
+
+    private void DeletePlatform(GameObject platformObject)
+    {
+        Destroy(platformObject);
+    }
+
+    private void AddNewCloud(GameObject[] platformObjects)
+    {
+        print("add new cloud");
+        GameObject highestPlatform = GetHighestCloud(platformObjects);
+        SpawnCloud(highestPlatform.transform.position.y);
+
+    }
+
+    private GameObject GetHighestCloud(GameObject[] cloudObjects)
+    {
+        GameObject highestCloud = cloudObjects[0];
+        foreach (GameObject cloudObject in cloudObjects)
+        {
+            if (cloudObject.transform.position.y > highestCloud.transform.position.y)
+                highestCloud = cloudObject;
+        }
+
+        return highestCloud;
     }
 }
+
+   // public float randomY = Random.Range(minimumYDistance, maximumYDistance) + lastPlatformY;
+
+    // spawn between z = 0 to 10
+    //               x = -4 to 4
+    //               y = 
